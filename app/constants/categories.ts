1d9401f4e492100e83b72edc4e05c7c41a0e55fd
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-07-27 17:52:28 -0500

    feat: Add constants for transaction, bill, and budget categories
    This commit adds a new file `categories.ts` under the `app/constants` directory. The file contains constants for transaction categories, bill categories, and budget categories. These constants provide predefined options for categorizing transactions, bills, and budgets in the application. This addition improves the maintainability and consistency of category-related functionality.

---
+++ app/constants/categories.ts
@@ -0,0 +1,23 @@
+// app/constants/categories.ts
+export const TRANSACTION_CATEGORIES = [
+    'Groceries',
+    'Utilities',
+    'Rent/Mortgage',
+    'Dining Out',
+    'Entertainment',
+    'Salary',
+    'Investments',
+    // ... more categories
+  ];
+  
+  export const BILL_CATEGORIES = [
+    'Utilities',
+    'Subscriptions',
+    'Insurance',
+    'Loans',
+    'Rent/Mortgage',
+    // ... more categories
+  ];
+  
+  // Note: BUDGET_CATEGORIES could be the same as TRANSACTION_CATEGORIES
+  export const BUDGET_CATEGORIES = TRANSACTION_CATEGORIES;
