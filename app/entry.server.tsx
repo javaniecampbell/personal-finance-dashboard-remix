21aaded846daff9959b0009a103dbaa1c443e77f
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-07-26 21:59:53 -0500

    feat: Import telemetry server module for collecting application metrics

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -3,7 +3,7 @@
  * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
  * For more information, see https://remix.run/file-conventions/entry.server
  */
-
+import './utils/telemetry.server';
 import { PassThrough } from "node:stream";
 
 import type { AppLoadContext, EntryContext } from "@remix-run/node";
