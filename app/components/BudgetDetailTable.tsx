dfd5a9c0b919309722c94341eee22a8560856308
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-07-28 00:47:55 -0500

    feat: Add isChangeRate prop to BudgetDetailTable component
    The BudgetDetailTable component has been updated to include the isChangeRate prop. When set to true, the component calculates and displays the change rate as a percentage of the budget amount. This enhancement provides valuable insights into the change in performance.

--- app/components/BudgetDetailTable.tsx
+++ app/components/BudgetDetailTable.tsx
@@ -1,16 +1,50 @@
+import { ArrowDownIcon, ArrowUpIcon } from "lucide-react";
 import React from "react";
 import type { Budget, BudgetPerformance } from "~/types";
 import { formatCurrency, formatPercentage } from "~/utils/formatters";
 
 type BudgetDetailTableProps = {
+  isChangeRate?: boolean;
   budgetOverview: {
     budgets: Budget[];
     performance: BudgetPerformance[];
   };
 };
 
+type BudgetDetailsRowProps = {
+  budget?: BudgetPerformance;
+};
+
+const BudgetDetailsRow: React.FC<BudgetDetailsRowProps> = ({ budget }) => {
+  const isOverBudget =
+    (budget?.actualAmount ?? 0) > (budget?.budgetedAmount ?? 0);
+  const performance =
+    (((budget?.actualAmount ?? 0) - (budget?.budgetedAmount ?? 0)) /
+      (budget?.budgetedAmount ?? 1)) *
+    100;
+
+  return (
+    <tr>
+      <td className="py-2">{budget?.name}</td>
+      <td className="text-right">{formatCurrency(budget?.budgetedAmount ?? 0)}</td>
+      <td className="text-right">{formatCurrency(budget?.actualAmount ?? 0)}</td>
+      <td
+        className={`flex items-center justify-end ${
+          isOverBudget ? "text-red-600" : "text-green-600"
+        }`}
+      >
+        <span className="flex items-center justify-between p-2">
+        {isOverBudget ? <ArrowUpIcon size={16} /> : <ArrowDownIcon size={16} />}
+        {formatPercentage(Math.abs(performance / 100))}
+        </span>
+      </td>
+    </tr>
+  );
+};
+
 const BudgetDetailTable: React.FC<BudgetDetailTableProps> = ({
   budgetOverview,
+  isChangeRate,
 }) => {
   const { budgets, performance } = budgetOverview;
 
@@ -31,6 +65,11 @@ const BudgetDetailTable: React.FC<BudgetDetailTableProps> = ({
             const budgetPerformance = performance.find(
               (p) => p.id === budget.id
             );
+            if (isChangeRate === true) {
+              return (
+                <BudgetDetailsRow key={budget.id} budget={budgetPerformance} />
+              );
+            }
             return (
               <tr key={budget.id}>
                 <td className="py-2">{budget.name}</td>
