23bfd63e68a89b06f74081b693827bc34056088a
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-07-29 12:36:54 -0500

    refactor: Update transactions component to allow for adding an account to a transaction

--- app/components/AddTransactionForm.tsx
+++ app/components/AddTransactionForm.tsx
@@ -1,14 +1,16 @@
-import React from "react";
-import { Form, useActionData } from "@remix-run/react";
+import React, { useState } from "react";
+import { Form, useNavigation } from "@remix-run/react";
 import { useFormState } from "~/hooks/useFormState";
-import { Transaction } from "~/types";
+import { Account, Transaction } from "~/types";
 import { TRANSACTION_CATEGORIES } from "~/constants/categories";
 
 const AddTransactionForm: React.FC<{
+  accounts?: Account[];
   onSubmit: (transactionData: Transaction) => void;
   onClose: () => void;
-}> = ({ onSubmit, onClose }) => {
-  const actionData = useActionData();
+}> = ({ onSubmit, onClose, accounts }) => {
+  const transition = useNavigation();
+  const [isTransfer, setIsTransfer] = useState(false);
   const { values, handleChange, handleSubmit, errors } = useFormState(
     {
       description: "",
@@ -39,21 +41,24 @@ const AddTransactionForm: React.FC<{
       <div className="space-y-4">
         <div>
           <label
-            htmlFor="description"
+            htmlFor="type"
             className="block text-sm font-medium text-gray-700"
           >
-            Description
+            Type
           </label>
-          <input
-            type="text"
-            id="description"
-            name="description"
-            value={values.description}
+          <select
+            id="type"
+            name="type"
+            value={values.type}
             onChange={handleChange}
-            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-          {errors.description && (
-            <p className="mt-2 text-sm text-red-600">{errors.description}</p>
+            className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+          >
+            <option value="expense">Expense</option>
+            <option value="income">Income</option>
+            <option value="transfer">Transfer</option>
+          </select>
+          {errors.type && (
+            <p className="mt-2 text-sm text-red-600">{errors.type}</p>
           )}
         </div>
         <div>
@@ -78,34 +83,21 @@ const AddTransactionForm: React.FC<{
         </div>
         <div>
           <label
-            htmlFor="category"
+            htmlFor="description"
             className="block text-sm font-medium text-gray-700"
           >
-            Category
+            Description
           </label>
-          {/* <input
+          <input
             type="text"
-            id="category"
-            name="category"
-            value={values.category}
+            id="description"
+            name="description"
+            value={values.description}
             onChange={handleChange}
             className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          /> */}
-          <select
-            name="category"
-            id="category"
-            onChange={handleChange}
-            value={values.category}
-            className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-          >
-            {TRANSACTION_CATEGORIES.map((category) => (
-              <option key={category} value={category}>
-                {category}
-              </option>
-            ))}
-          </select>
-          {errors.category && (
-            <p className="mt-2 text-sm text-red-600">{errors.category}</p>
+          />
+          {errors.description && (
+            <p className="mt-2 text-sm text-red-600">{errors.description}</p>
           )}
         </div>
         <div>
@@ -129,32 +121,96 @@ const AddTransactionForm: React.FC<{
         </div>
         <div>
           <label
-            htmlFor="type"
+            htmlFor="accountId"
             className="block text-sm font-medium text-gray-700"
           >
-            Type
+            {isTransfer ? "From Account" : "Account"}
           </label>
           <select
-            id="type"
-            name="type"
-            value={values.type}
-            onChange={handleChange}
+            id="accountId"
+            name="accountId"
+            required
             className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+            onChange={(e) => {
+              e.preventDefault();
+              setIsTransfer(e.target.value === "transfer");
+              handleChange(e);
+            }}
           >
-            <option value="expense">Expense</option>
-            <option value="income">Income</option>
+            {accounts?.map((account) => (
+              <option key={account.id} value={account.id}>
+                {account.name}
+              </option>
+            ))}
           </select>
-          {errors.type && (
-            <p className="mt-2 text-sm text-red-600">{errors.type}</p>
+          {errors.accountId && (
+            <p className="mt-2 text-sm text-red-600">{errors.accountId}</p>
           )}
         </div>
+
+        {isTransfer && (
+          <div>
+            <label
+              htmlFor="toAccountId"
+              className="block text-sm font-medium text-gray-700"
+            >
+              To Account
+            </label>
+            <select
+              id="toAccountId"
+              name="toAccountId"
+              required
+              className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+            >
+              {accounts?.map((account) => (
+                <option key={account.id} value={account.id}>
+                  {account.name}
+                </option>
+              ))}
+            </select>
+          </div>
+        )}
+        {!isTransfer && (
+          <div>
+            <label
+              htmlFor="category"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Category
+            </label>
+            {/* <input
+              type="text"
+              name="category"
+              id="category"
+              required
+              className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            /> */}
+            <select
+              name="category"
+              id="category"
+              required
+              onChange={handleChange}
+              value={values.category}
+              className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+            >
+              {TRANSACTION_CATEGORIES.map((category) => (
+                <option key={category} value={category}>
+                  {category}
+                </option>
+              ))}
+            </select>
+          </div>
+        )}
       </div>
       <div className="mt-5 sm:mt-6">
         <button
           type="submit"
+          disabled={transition.state === "submitting"}
           className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
         >
-          Add Transaction
+          {transition.state === "submitting"
+            ? "Submitting..."
+            : "Create Transaction"}
         </button>
       </div>
     </Form>
