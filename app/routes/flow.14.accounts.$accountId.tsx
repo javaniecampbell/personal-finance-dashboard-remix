98cab9c1ddbf93a24997d24079db88084fdb6b20
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-08-02 17:57:06 -0500

    fix: Transactions failing to load due to nesting of transactions data

--- app/routes/flow.14.accounts.$accountId.tsx
+++ app/routes/flow.14.accounts.$accountId.tsx
@@ -19,7 +19,7 @@ export const loader: LoaderFunction = async ({ request, params }) => {
     getAccount(userId, accountId),
     getTransactions(userId, {
       accountId,
-      limit: 10,
+      limit: 30,
       filterType: "all",
       sortBy: "date",
       page: 1,
@@ -37,18 +37,17 @@ export const loader: LoaderFunction = async ({ request, params }) => {
 export default function AccountDetailPage() {
   const { account, transactions } = useLoaderData<typeof loader>();
   const { accountId } = useParams();
-
   return (
     <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 px-1">
       <h1 className="text-2xl font-semibold text-gray-900 mb-6">{`Account Details ${
         accountId ? "| " + accountId : ""
       } `}</h1>
-      <AccountDetails account={account} transactions={transactions} />
+      <AccountDetails account={account} transactions={transactions.transactions} />
       <div className="mt-8">
         <h2 className="text-xl font-semibold text-gray-900 mb-4">
           Transaction History
         </h2>
-        <TransactionList transactions={transactions} />
+        <TransactionList transactions={transactions.transactions} />
       </div>
     </div>
   );
