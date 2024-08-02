c334f5762d74ef502a34ba0148c15e870cff0499
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-08-02 16:53:03 -0500

    fix: Ensures that the user is validated from database not in memory

--- app/routes/flow.14.accounts.$accountId.tsx
+++ app/routes/flow.14.accounts.$accountId.tsx
@@ -1,7 +1,7 @@
 // app/routes/accounts/$accountId.tsx
 import { json, LoaderFunction } from "@remix-run/node";
 import { useLoaderData, useParams } from "@remix-run/react";
-import { requireUserId } from "~/utils/auth.server";
+import { requireUserId } from "~/utils/auth.server.v2";
 import { getAccount } from "~/utils/accounts.server";
 import { getTransactions } from "~/utils/transactions.server";
 import { AccountDetails } from "~/components/AccountDetails";
@@ -39,7 +39,7 @@ export default function AccountDetailPage() {
   const { accountId } = useParams();
 
   return (
-    <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+    <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 px-1">
       <h1 className="text-2xl font-semibold text-gray-900 mb-6">{`Account Details ${
         accountId ? "| " + accountId : ""
       } `}</h1>
