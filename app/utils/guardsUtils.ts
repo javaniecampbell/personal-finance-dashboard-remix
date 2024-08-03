64d1bfc0e50c591e4635a6472feeafcfeb4a0820
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-08-02 23:48:29 -0500

    feat: adds a helper for isEmpty guard

---
+++ app/utils/guardsUtils.ts
@@ -0,0 +1,19 @@
+// create a method to check if value is Empty, string, number, array or object
+export function isEmpty(value: unknown): boolean {
+    if (value === null || value === undefined) {
+        return true;
+    }
+    if (typeof value === 'string') {
+        return value.trim().length === 0;
+    }
+    if (typeof value === 'number') {
+        return value !== 0;
+    }
+    if (Array.isArray(value)) {
+        return value.length === 0;
+    }
+    if (typeof value === 'object') {
+        return Object.keys(value).length === 0;
+    }
+    return false;
+}
